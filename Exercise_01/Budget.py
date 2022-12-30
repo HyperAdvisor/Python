ee820aa10bb27d1b5d193deabb58125b342b787d
Declan Doherty <jack.doherty@ericsson.com>
2022-12-30 13:09:58 +0000

    add exercise 1

---
+++ Exercise_01/Budget.py
@@ -0,0 +1,10 @@
+Budget = 2000
+CostOfCourse = 500
+CostOfLaptop = 800
+CostofCoffeeAndSnacks = 5
+NumberOfClasses = 40
+
+TotalCostOfSnacks = CostofCoffeeAndSnacks * NumberOfClasses
+TotalCost = TotalCostOfSnacks + CostOfCourse + CostOfLaptop
+Surplus = Budget - TotalCost
+print(Surplus)
